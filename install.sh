c5796add19cc15631dc3b7ff7e9b95d7bba940d3
Katryoshkh <riyadifarras@gmail.com>
2025-05-02 09:18:49 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/system/bin/sh
+
+SKIPMOUNT=false
+PROPFILE=false
+POSTFSDATA=true
+LATESTARTSERVICE=true
+
+print_modname() {
+  ui_print "                 "
+  ui_print " Module to resize ZRAM to 4 GB"
+  ui_print " Created by Katryoshkh"
+  ui_print "                 "
+}
+
+set_permissions() {
+  set_perm_recursive $MODPATH 0 0 0755 0644
+  set_perm $MODPATH/service.sh 0 0 0777
+}
+
+set_permissions
+
+ui_print "ZRAM module installed successfully!"
